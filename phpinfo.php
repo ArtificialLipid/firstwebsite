7a48b866247e459188573e521996b4a7b1e24cbb
S u m t e r <sumteralton@gmail.com>
2019-07-22 11:05:31 -0400

    add phpinfo

---
+++ phpinfo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Fuck yeah, PHP!</title>
+    </head>
+    <body>
+        <h1>Y E A H</h1>
+    <?php
+    echo 'Hello world!';
+    ?>
+</body>
+</html>
