8f5803725380c58739ca9451fa4cfe65caf9d379
S u m t e r <sumteralton@gmail.com>
2019-07-23 10:11:17 -0400

    mess around

--- phpinfo.php
+++ phpinfo.php
@@ -6,7 +6,15 @@
     <body>
         <h1>Y E A H</h1>
     <?php
+    $benis = 'benis';
+    $x = 10.53412;
+    define('GURT', ['weengis', 'chud', 'glorpman'], true);
+
+    echo gurt[2] . '<br />';
+    echo var_dump($x);
     echo 'Hello world!';
+    echo "<h2>" . strrev($benis) . "</h2>";
+    echo $_POST['thing'];
     ?>
 </body>
 </html>
